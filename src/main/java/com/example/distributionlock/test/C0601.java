54a296ff36d410d58444968523e41a79e37f421d
kezhang95 <1987398226@qq.com>
2023-06-01 16:01:30 +0800

    本地新建了一个类C0601

---
+++ src/main/java/com/example/distributionlock/test/C0601.java
@@ -0,0 +1,14 @@
+package com.example.distributionlock.test;
+
+/**
+ * @Package: com.example.distributionlock.test
+ * @ClassName: C0601
+ * @Author: k.z
+ * @CreateTime: 2023/6/1 15:59
+ * @Description:
+ */
+public class C0601 {
+    public static void main(String[] args) {
+        System.out.println("随便建了一个类");
+    }
+}
