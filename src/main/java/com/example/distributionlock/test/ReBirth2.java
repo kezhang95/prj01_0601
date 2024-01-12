3445ceea5c966add633910f22a0d02998a587083
kezhang95 <1987398226@qq.com>
2024-01-12 14:31:10 +0800

    2024-01-12新提交一个类new feature

---
+++ src/main/java/com/example/distributionlock/test/ReBirth2.java
@@ -0,0 +1,14 @@
+package com.example.distributionlock.test;
+
+/**
+ * @Package: com.example.distributionlock.test
+ * @ClassName: ReBirth2
+ * @Author: k.z
+ * @CreateTime: 2024/1/12 14:28
+ * @Description:
+ */
+public class ReBirth2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!HaHa");
+    }
+}
